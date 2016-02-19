411f6b044c680bc1a6b88c3649c557cab3b959ef
J. "Marahin" M <me@marahin.pl>
2016-02-19 22:54:32 +0100

    debug

--- data/chatchannels/scripts/help.lua
+++ data/chatchannels/scripts/help.lua
@@ -1,5 +1,5 @@
 local CHANNEL_HELP = 7
---[[
+
 function createMutedCondition( time )
 	if not (type(time) == "number" and type =~ nil and type > 0) then
 		time = 60000
@@ -10,7 +10,7 @@ function createMutedCondition( time )
 	
 	return muted
 end
-]]
+
 function onSpeak(player, type, message)
 	local playerAccountType = player:getAccountType()
 	if player:getLevel() == 1 and playerAccountType == ACCOUNT_TYPE_NORMAL then
