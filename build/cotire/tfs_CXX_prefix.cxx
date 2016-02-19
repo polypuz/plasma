3f2a37ea668998f8a9ed07a6c60cf8212afcd2a9
Andrzej Voss <git@poczta.tril.pl>
2016-02-19 17:46:19 +0100

    caly build/ powinien byc ignorowany.

--- build/cotire/tfs_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.6 generated file */
-/* /home/marahin/forgottenserver/build/cotire/tfs_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/home/marahin/forgottenserver/src/otpch.h"
-#endif
