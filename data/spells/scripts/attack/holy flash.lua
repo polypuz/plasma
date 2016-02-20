e4cbf70e9281426f1f2d3a734d17e762bd685e54
mayoseh <enovaacodes@gmail.com>
2016-02-20 17:09:26 +0100

    Utori San i Envenom fixes #17
    Niech ktoś znający się na czarach sprawdzi, czy holy flash jest dobrze
    napisany, formułka na ilość ticków:
    7+mlvl/2, zwiększone obrażenia z 10 na 20, częstsze ticki
    
    Envenom - cały czas biło po 45 (obrażenia nie malały, zostało
    zapożyczone z curse)
    
    Curse - zmniejszona ilość ticków (chyba o 10, zmniejszona ilość ticków
    tych mniejszych obrażeń, bije tyle co envenom)

--- data/spells/scripts/attack/holy flash.lua
+++ data/spells/scripts/attack/holy flash.lua
@@ -3,9 +3,16 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
 
+local ticks = 0
+function onGetFormulaValues(player, level, maglevel)
+	ticks = 7+(maglevel / 2)
+	return 0,0
+end
+combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
+
 local condition = Condition(CONDITION_DAZZLED)
 condition:setParameter(CONDITION_PARAM_DELAYED, true)
-condition:addDamage(50, 10000, -10)
+condition:addDamage(ticks, 5000, -20)
 combat:setCondition(condition)
 
 function onCastSpell(creature, variant)
