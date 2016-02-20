bc1d88d01a355b6e9827e1429a37adf2befeb514
mayoseh <enovaacodes@gmail.com>
2016-02-20 21:14:08 +0100

    Utori San fix #17
    Próba nr 3

--- data/spells/scripts/attack/holy flash.lua
+++ data/spells/scripts/attack/holy flash.lua
@@ -3,19 +3,12 @@ combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
 combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
 
-local ticks = 0
-function onGetFormulaValues(player, level, maglevel)
-	ticks = 7+(maglevel / 2)
-	return -20, -20
-end
-combat:setCallback(CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
-
 local condition = Condition(CONDITION_DAZZLED)
 condition:setParameter(CONDITION_PARAM_DELAYED, true)
-print("HolyFlash Ticks: " .. tostring(ticks))
-condition:addDamage(ticks, 5000, -20)
-combat:setCondition(condition)
 
 function onCastSpell(creature, variant)
+    local repeats = (creature:getMagicLevel() / 2) + 7
+    condition:addDamage(repeats, 5000, -20)
+	combat:setCondition(condition)
 	return combat:execute(creature, variant)
 end
