9b6d6d314fb86fac6e132a231cb6cd1e9ebddd7d
mayoseh <enovaacodes@gmail.com>
2016-02-20 21:48:33 +0100

    Utori San fix #17
    kolejna wersja

--- data/spells/scripts/attack/holy flash.lua
+++ data/spells/scripts/attack/holy flash.lua
@@ -1,12 +1,15 @@
-local combat = Combat()
-combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
-combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
-combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
-
-local condition = Condition(CONDITION_DAZZLED)
-condition:setParameter(CONDITION_PARAM_DELAYED, true)
-
 function onCastSpell(creature, variant)
+	local combat = nil
+
+	combat = Combat()
+	combat:setParameter(COMBAT_PARAM_TYPE, COMBAT_HOLYDAMAGE)
+	combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_HOLYDAMAGE)
+	combat:setParameter(COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_SMALLHOLY)
+
+	local condition = nil
+	condition = Condition(CONDITION_DAZZLED)
+	condition:setParameter(CONDITION_PARAM_DELAYED, true)
+
     local repeats = (creature:getMagicLevel() / 2) + 7
     condition:addDamage(repeats, 5000, -20)
 	combat:setCondition(condition)
