e74d64a8ec660cc0a3aa61ede5a526545961c1c5
J. "Marahin" M <me@marahin.pl>
2016-02-21 20:32:57 +0100

    quest verification (if player has completed it already)

--- data/movements/scripts/martwedrzewa.lua
+++ data/movements/scripts/martwedrzewa.lua
@@ -1,3 +1,19 @@
+local function hasCompletedQuest( cid )
+	-- these are also defined in data/actions/scripts/quests/martwedrzewa.lua
+	-- and are set when you receive a reward from any of the chests
+	local generic_reward_storage_key = 30024
+	local unique_storage_key = 30025
+	-- end of the storage keys
+	
+	local p = Player( cid )
+	
+	if p:getStorageValue( generic_reward_storage_key ) == 1 or p:getStorageValue( unique_storage_key ) == 1 then
+		return true
+	else
+		return false
+	end
+end
+
 function onStepIn(cid, item, position, to)
 	if item.actionid == 36000 then
 		--doCreatureSay(cid, 'X', TALKTYPE_ORANGE_1)
@@ -6,7 +22,7 @@ function onStepIn(cid, item, position, to)
 		local failure = {x = 893, y = 2035, z = 10}
 		local dest = { }
 
-		if cid:getStorageValue(36900) == 2 then
+		if cid:getStorageValue(36900) == 2 and not hasCompletedQuest( cid ) then
 			dest = success
 		else
 			dest = failure
