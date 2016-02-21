66863e4ff7aeb399e61f0ab9b266b4c18c5dad7a
J. "Marahin" M <me@marahin.pl>
2016-02-21 16:09:43 +0100

    debug info

--- data/actions/scripts/quests/quests.lua
+++ data/actions/scripts/quests/quests.lua
@@ -1,5 +1,6 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	if item.uid <= 1000 or item.uid > 22700 then
+		print("Chest has no uid matching, giving nothing.")
 		return false
 	end
 
@@ -8,6 +9,7 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local playerCap = player:getFreeCapacity()
 	if player:getStorageValue(item.uid) == -1 then
 		if playerCap >= itemWeight then
+			print("Chest has uid " .. tostring( item.uid ) .. ". Giving an item.")
 			player:sendTextMessage(MESSAGE_INFO_DESCR, 'You have found a ' .. itemType:getName() .. '.')
 			player:addItem(item.uid, 1)
 			player:setStorageValue(item.uid, 1)
