97fb7680375c24abd8a911d3f1edb3a20ee00c57
mayoseh <enovaacodes@gmail.com>
2016-02-21 14:18:38 +0100

    #30 #29 poprawki do poprawek

--- data/talkactions/scripts/guild.lua
+++ data/talkactions/scripts/guild.lua
@@ -3,6 +3,7 @@ function onSay(player, words, param)
 	local players = Game.getPlayers()
 	local playerCount = Game.getPlayerCount()
 
+	local vocNames = {" S", " D", " P", " K", " MS", " ED", " RP", " EK"}
 	local playerGuild = player:getGuild()
 
 	local i = 0
@@ -18,17 +19,12 @@ function onSay(player, words, param)
 							msg = msg .. ", "
 						end
 
-						local voc = ""
-						if targetPlayer:getVocation():getId() == 1 then voc = " S"
-						elseif targetPlayer:getVocation():getId() == 2 then voc = " D"
-						elseif targetPlayer:getVocation():getId() == 3 then voc = " P"
-						elseif targetPlayer:getVocation():getId() == 4 then voc = " K"
-						elseif targetPlayer:getVocation():getId() == 5 then voc = " MS"
-						elseif targetPlayer:getVocation():getId() == 6 then voc = " ED"
-						elseif targetPlayer:getVocation():getId() == 7 then voc = " RP"
-						elseif targetPlayer:getVocation():getId() == 8 then voc = " EK"
-							
-						msg = msg .. targetPlayer:getName() .. " [" .. targetPlayer:getLevel() .. voc .. "]"
+						if targetPlayer:getVocation():getId() > 0 and targetPlayer:getVocation():getId() < 9 then
+							msg = msg .. targetPlayer:getName() .. " [" .. targetPlayer:getLevel() .. vocNames[targetPlayer:getVocation():getId()] .. "]"
+						else
+							msg = msg .. targetPlayer:getName() .. " [" .. targetPlayer:getLevel() .. "]"
+						end
+
 						i = i + 1
 					end
 				end
