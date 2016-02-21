dcaafff965ea586d867e85a5c56b39807f8316ae
J. "Marahin" M <me@marahin.pl>
2016-02-21 03:44:06 +0100

    scaling the damage

--- data/actions/scripts/quests/martwedrzewa.lua
+++ data/actions/scripts/quests/martwedrzewa.lua
@@ -4,12 +4,15 @@ function onUse(cid, item, position, target, pos)
 		return false
 	end
 	
+	local p_maxhealth = getCreatureMaxHealth( cid )
+	
 	if item.uid > 36000 and item.uid < 36006 then
 		print("Gracz " .. Player(cid):getName() .. " atakuje drzewo")
 		if cid:getStorageValue(item.uid) ~= 1 then
 			print("Gracz " .. Player(cid):getName() .. " dokonal ataku ")
 			if math.random() > 0.1 then
-				doTargetCombatHealth(0, cid, 1, -200, -750,  1)
+				--doTargetCombatHealth(0, cid, 1, -200, -750,  1)
+				doTargetCombatHealth(0, cid, 1, p_maxhealth * 0.1 * -1, math.random() * p_maxhealth * -1, 1)
 				doSendMagicEffect(position, CONST_ME_BLOCKHIT)
 			else
 				doCreatureSay(cid, 'Achhh...', TALKTYPE_ORANGE_1)
