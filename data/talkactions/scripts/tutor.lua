1a8b38cb32921625a851f3cdfc23b4fba6286253
mayoseh <enovaacodes@gmail.com>
2016-02-21 13:40:42 +0100

    komenda !tutors, fix #31
    nie jestem specem od funkcji w silniku więc do przetestowania

---
+++ data/talkactions/scripts/tutor.lua
@@ -0,0 +1,36 @@
+function onSay(player, words, param)
+	local hasAccess = player:getGroup():getAccess()
+	local players = Game.getPlayers()
+	local playerCount = Game.getPlayerCount()
+
+	local i = 0
+	local msg = "Tutors online: "
+	for k, targetPlayer in ipairs(players) do
+		if hasAccess or not targetPlayer:isInGhostMode() then
+			if targetPlayer:getAccountType() == ACCOUNT_TYPE_TUTOR then
+				if i > 0 then
+					msg = msg .. ", "
+				end
+				msg = msg .. targetPlayer:getName()
+				i = i + 1
+			end
+		end
+
+		if i == 10 then
+			if k == playerCount then
+				msg = msg .. "."
+			else
+				msg = msg .. ","
+			end
+			player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, msg)
+			msg = ""
+			i = 0
+		end
+	end
+
+	if i > 0 then
+		msg = msg .. "."
+		player:sendTextMessage(MESSAGE_STATUS_CONSOLE_BLUE, msg)
+	end
+	return false
+end
