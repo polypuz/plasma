a9a5ec953aaa355bb4d586895642fbafca599f82
J. "Marahin" M <me@marahin.pl>
2016-02-21 02:29:45 +0100

    teleporting fix

--- data/npc/scripts/czarnobrody.lua
+++ data/npc/scripts/czarnobrody.lua
@@ -2,8 +2,6 @@ local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-local destination = {}
-
 function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
 function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
 function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
@@ -13,7 +11,13 @@ local function greetCallback(cid)
 	return true
 end
 
+local function teleportPlayer( cid, dest )
+	Player(cid):teleportTo( Position( dest ) )
+	return true
+end
+
 local function creatureSayCallback(cid, type, msg)
+	local destination = {}
 	if not npcHandler:isFocused(cid) then
 		return false
 	elseif msgcontains(msg, "transport") then
@@ -22,10 +26,11 @@ local function creatureSayCallback(cid, type, msg)
 			npcHandler:say("Jestes pewny, ze chcesz plynac do Mirko Town?", cid)
 			npcHandler.topic[cid] = 1
 	elseif (msgcontains(msg, "tak") or msgcontains(msg, "yes")) and npcHandler.topic[cid] == 1 then
-		local destination = {x = 1011, y = 1022, z = 6}
+		destination = {x = 1011, y = 1022, z = 6}
 		npcHandler:releaseFocus(cid)
 		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_TELEPORT)
-		doTeleportThing(cid, destination)
+		--doTeleportThing(cid, destination)
+		teleportPlayer(cid, destination)
 		doSendMagicEffect(destination, CONST_ME_TELEPORT)
 	elseif (msgcontains(msg, "nie") or msgcontains(msg, "no")) and npcHandler.topic[cid] == 1 then
 		npcHandler:say("To sie psia mac zdecyduj.", cid)
@@ -33,10 +38,11 @@ local function creatureSayCallback(cid, type, msg)
 		npcHandler:say("Na pewno chcesz plynac do Suwalek? *usmiecha sie* Ta nazwa nie jest przypadkowa.", cid)
 		npcHandler.topic[cid] = 2
 	elseif (msgcontains(msg, "tak") or msgcontains(msg, "yes")) and npcHandler.topic[cid] == 2 then
-		local destination = { x= 901, y = 2122, z = 6 }
+		destination = { x= 901, y = 2122, z = 6 }
 		npcHandler:releaseFocus(cid)
 		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_TELEPORT)
-		doTeleportThing(cid, destination)
+		--doTeleportThing(cid, destination)
+		teleportPlayer(cid, destination)
 		doSendMagicEffect(destination, CONST_ME_TELEPORT)
 	elseif (msgcontains(msg, "nie") or msgcontains(msg, "no")) and npcHandler.topic[cid] == 2 then
 		npcHandler:say("To sie psia mac zdecyduj.", cid)
