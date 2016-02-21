189e11c93502848ab63bf66b96c65c628ee8f6c6
J. "Marahin" M <me@marahin.pl>
2016-02-21 12:55:06 +0100

    fixing the teleport coordinates
    ... for success / fail locations on Blisher Quest

--- data/movements/scripts/martwedrzewa.lua
+++ data/movements/scripts/martwedrzewa.lua
@@ -2,8 +2,8 @@ function onStepIn(cid, item, position, to)
 	if item.actionid == 36000 then
 		--doCreatureSay(cid, 'X', TALKTYPE_ORANGE_1)
 
-		local success = {x = 1003, y = 824, z = 7}
-		local failure = {x = 1017, y = 826, z = 7}
+		local success = {x = 894, y = 2028, z = 12}
+		local failure = {x = 893, y = 2035, z = 10}
 		local dest = { }
 
 		if cid:getStorageValue(36900) == 2 then
