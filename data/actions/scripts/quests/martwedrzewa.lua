c3a63fc5d6c4c250dafd23f4ad54b804f5bfaf19
J. "Marahin" M <me@marahin.pl>
2016-02-21 04:12:49 +0100

    fixing boss fight

--- data/actions/scripts/quests/martwedrzewa.lua
+++ data/actions/scripts/quests/martwedrzewa.lua
@@ -1,37 +1,46 @@
+local function checkIfFoughtTrees( cid )
+	local dead_trees_tbl = [36901, 36902, 36903, 36904, 36905]
+
+	for key,value in pairs(dead_trees_tbl) do --actualcode
+		if cid:getStorageValue( value ) ~= 1 then
+			return false
+		end
+	end
+	
+	return true
+end
+
 function onUse(cid, item, position, target, pos)
 	if cid:getStorageValue(36900) ~= 1 and cid:getStorageValue(36900) ~= 2 then
-		print("Gracz " .. Player(cid):getName() .. " nie wzial questa.")
 		return false
 	end
 	
 	local p_maxhealth = getCreatureMaxHealth( cid )
 	
 	if item.uid > 36000 and item.uid < 36006 then
-		print("Gracz " .. Player(cid):getName() .. " atakuje drzewo")
 		if cid:getStorageValue(item.uid) ~= 1 then
-			print("Gracz " .. Player(cid):getName() .. " dokonal ataku ")
 			if math.random() > 0.1 then
 				--doTargetCombatHealth(0, cid, 1, -200, -750,  1)
-				doTargetCombatHealth(0, cid, 1, p_maxhealth * 0.1 * -1, math.random() * p_maxhealth * -1, 1)
+				doTargetCombatHealth(0, cid, 1, p_maxhealth * 0.1 * -1, math.random(0.11, 0.55) * p_maxhealth * -1, 1)
 				doSendMagicEffect(position, CONST_ME_BLOCKHIT)
 			else
-				doCreatureSay(cid, 'Achhh...', TALKTYPE_ORANGE_1)
+				doCreatureSay(cid, '*trzask*', TALKTYPE_ORANGE_1)
 				doSendMagicEffect(position, 49)
 				cid:setStorageValue(item.uid, 1)
 			end
 		else
-			print("Gracz " .. Player(cid):getName() .. " bije drzewo ktore juz zniszczyl.")
 			doSendMagicEffect(position, CONST_ME_POFF)
 		end
 	elseif item.uid == 36006 then -- boss
-		print("Gracz " .. Player(cid):getName() .. " atakuje bossa.")
-		if cid:getStorageValue(36900) == 2 then
+		if cid:getStorageValue(36900) == 2 or checkIfFoughtTrees( cid ) then
 			if cid:getStorageValue(36006) ~= 1 then
 				if math.random() > 0.1 then
-					doTargetCombatHealth(0, cid, 1, -800, -1400, 1)
+					--doTargetCombatHealth(0, cid, 1, -800, -1400, 1)
+					doTargetCombatHealth(0, cid, 1, p_maxhealth * 0.35 * -1, math.random(0.11, 0.95) * p_maxhealth * -1, 1)
+
 					doSendMagicEffect(position, CONST_ME_BLOCKHIT)
 				else
-					doCreatureSay(cid, 'Achhh...', TALKTYPE_ORANGE_1)
+					doCreatureSay(cid, '*trzask*', TALKTYPE_ORANGE_1)
 					doSendMagicEffect(position, 49)
 					cid:setStorageValue(36006, 1)
 				end
@@ -40,13 +49,7 @@ function onUse(cid, item, position, target, pos)
 			end
 		end
 	else
-		print("Gracz " .. Player(cid):getName() .. " klika na czyms, co nie jest tym obiektem.")
 	end
 	
-	
-	print("Gracz " .. Player(cid):getName() .. " - po prostu zwracam wartosc.")
-	print("item.uid" .. item.uid)
-	print("36900 storage:" .. cid:getStorageValue( 36900))
-	print("36906 storage:" .. cid:getStorageValue( 36906))
 	return true
 end
