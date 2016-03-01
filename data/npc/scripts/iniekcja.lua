cf9925a2dc33bd03330186772edccd9be19e1a80
Jasiek ~marahin Matusz <me@marahin.pl>
2016-03-01 03:50:14 +0100

    dodanie rpg i poprawki u iniekcji, dodane opisy przedmiotow questowych

--- data/npc/scripts/iniekcja.lua
+++ data/npc/scripts/iniekcja.lua
@@ -11,11 +11,10 @@ local function greetCallback(cid)
 	return true
 end
 
-local alreadyTensed = false
-
 local function creatureSayCallback(cid, type, msg)
-	local storageValGood = 39650 -- zakon 4konserw
-  local storageValBad = 39651 -- zakon Neuropy
+	local storageValGood = 39651 -- zakon 4konserw
+  local storageValBad = 39650 -- zakon Neuropy
+
   --[[
     -1 = haven't taken the quests
     0 = have taken the recruitment quests
@@ -30,23 +29,16 @@ local function creatureSayCallback(cid, type, msg)
 	if not npcHandler:isFocused(cid) then
 		return false
   elseif player:getStorageValue( storageValBad ) ~= -1 then
-		if alreadyTensed then
-			doTargetCombatHealth(0, cid, 1, player:getHealth() * 0.1 * -1, math.random(0.11, 0.76) * player:getHealth() * -1, 1)
-			npcHandler:say("OSTRZEGALEM. ODEJDZ.", cid)
-		else
-			npcHandler:say("Radze odejsc, " .. player:getName() .. ", nie zwyklem tolerowac neuropejczykow.", cid)
-			alreadyTensed = true
-			print("Already tensed? " .. tostring(alreadyTensed))
-		end
-  elseif (msgcontains(msg, "zakon") or msgcontains(msg, "4konserwy") or msgcontains(msg, "4konserw")) and player:getStorageValue( storageValGood ) < 0 then
-    npcHandler:say({"Zakon 4konserw jest ostatnimi czasy oslabiony, a z tego co wiem - jestem jedynym ocalalym nauczycielem.", "Kazdy z czlonkow jednak - musi byc pelen cnot, znac i przestrzegac 3 aksjomat, wierzyc w Krula do samego konca.", "Aby dolaczyc do {Zakonu} musisz jednak udowodnic, ze jestes tego warty. Czy jestes zainteresowany, " .. Player(cid):getName() .. "?"}, cid)
+    npcHandler:say("ODEJDZ, " .. player:getName() .. ", CHYBA, ZE CHCESZ POSLUCHAC O MARKSIE.", cid)
+  elseif (msgcontains(msg, "zakon") or msgcontains(msg, "neuropa") or msgcontains(msg, "neuropy")) and player:getStorageValue( storageValGood ) < 0 then
+    npcHandler:say({"Zakon Neuropy jest silnym stowarzyszeniem, ktore ma na celu promowanie wspoldzielenia dachow w Mirko Town, wspoldzielenia zlota i pobierania podatku od najmozniejszych u Rysia, szerzenia serwera MirkOTS na OTServlist.", "Kazdy z czlonkow jednak - musi byc krewnym Klejnotu Nilu, miec pochodzenie wlascicieli kamienicy, albo chociaz byc gejem, stylista, afroamerykaninem albo feminista. No i nie mozesz byc fashysta.", "Aby dolaczyc do {Zakonu} musisz udowodnic, ze jestes tego warty. Bynajmniej nie chodzi o szekle. Czy jestes zainteresowany, " .. Player(cid):getName() .. "?"}, cid)
     npcHandler.topic[cid] = 1
   elseif (msgcontains(msg, "yes") or msgcontains(msg, "tak")) and npcHandler.topic[cid] == 1 then
-    npcHandler:say({"Musze Cie jednak ostrzec: nasi odwieczni przeciwnicy, {Zakon Neuropy} nie beda chcieli z Toba rozmawiac, gdy do nas dolaczysz. Jakikolwiek kontakt, jestem prawie pewny, skonczy sie smiertelna konfrontacja.", "Czy nadal jestes pewny swojej decyzji?"}, cid)
+    npcHandler:say({"Musze Cie jednak ostrzec: nasi odwieczni przeciwnicy, {Zakon 4konserw}, nie beda chcieli z Toba rozmawiac, gdy do nas dolaczysz. Jakikolwiek kontakt, jestem prawie pewny, skonczy sie smiertelna konfrontacja.", "Czy nadal jestes pewny swojej decyzji?"}, cid)
     npcHandler.topic[cid] = 2
   elseif (msgcontains(msg, "nie") or msgcontains(msg, "no")) and npcHandler.topic[cid] == 1 then
     -- no @ 1
-    npcHandler:say({"Rozumiem. W takim razie idz w pokoju."}, cid)
+    npcHandler:say({"Rozumiem. W takim razie idz w wuj, fashysto."}, cid)
     npcHandler.topic[cid] = 0
   elseif (msgcontains(msg, "tak") or msgcontains(msg, "yes")) and npcHandler.topic[cid] == 2 then
     npcHandler:say({"W porzadku. Aby jednak udowodnic swoja wartosc, przynies mi 500 {Demonic Essence}. To jest Twoje pierwsze {zadanie}. Zycze powodzenia."}, cid)
@@ -72,15 +64,15 @@ local function creatureSayCallback(cid, type, msg)
       end
     else
       if player:getStorageValue(storageValGood) > 4 then
-        npcHandler:say("Niestety, ale nie mam dla Ciebie wiecej zadan. Jestes konserwa najwyzszej rangi.", cid)
+        npcHandler:say("Niestety, ale nie mam dla Ciebie wiecej zadan. Jestes neuropkiem najwyzszej rangi.", cid)
       else
         if player:getStorageValue(storageValGood) < 0 then
           npcHandler:say("Twoim pierwszym zadaniem bedzie przyniesienie 500 {Demonic Essence}. Gdy to zrobisz, przyjdz do mnie.", cid)
         elseif player:getStorageValue( storageValGood ) < 2 then
-          npcHandler:say("Zakon 4konserw potrzebuje duzej ilosci {Demonic Essence}, aby zasilac nasza wiare w Krula. Przynies mi kolejny 1000 {Demonic Essence}, a zostaniesz nagrodzony.", cid)
+          npcHandler:say("Zakon Neuropy potrzebuje duzej ilosci {Demonic Essence}, zeby miec czym grzac na squotach. Przynies mi kolejne 1000 {Demonic Essence}, a zostaniesz nagrodzony.", cid)
           player:setStorageValue( storageValGood, 2 )
         elseif player:getStorageValue( storageValGood ) < 4 then
-          npcHandler:say("To bedzie Twoje ostatnie zadanie. Po jego wykonaniu - awansujesz na starszego oficera 4konserw. Aby je wykonac, przynies mi 1500 {Demonic Essence}, a bedzie Ci dana relikwia poswiecona przez autora 3 Aksjomatu.", cid)
+          npcHandler:say("To bedzie Twoje ostatnie zadanie. Po jego wykonaniu - awansujesz na starszego szeklomajstra. Aby je wykonac, przynies mi 1500 {Demonic Essence}, a bedzie Ci dana tarcza antyfashysty, potrzebna do zwalczania zlych konserw.", cid)
           player:setStorageValue( storageValGood, 4)
         end
       end
@@ -88,10 +80,10 @@ local function creatureSayCallback(cid, type, msg)
   elseif (msgcontains(msg, "tak") or msgcontains(msg, "yes")) and npcHandler.topic[cid] >= 10 then
     if npcHandler.topic[cid] == 10 then
       if player:removeItem(6500, 500) then
-        npcHandler:say({"Niechaj bedzie! W imieniu 4konserw, mianuje Cie rycerzem 3 aksjo, poplecznikiem Krula. Zakon, szkola, strzelnica!", "Przydzielona Ci zostala ta oto zbroja, korzystaj z niej madrze. Kiedy bedziesz gotowy, zglos sie po kolejne {zadanie}."}, cid)
+        npcHandler:say({"No spoko, jako mistrz tego squatu oficjalnie mianuje Cie neuropkiem-antyfashysta.", "Masz tutaj troche lachmanow, przydadza Ci sie zebys wygladal jak reszta dobrych chlopakow. Kiedy bedziesz gotowy, zglos sie po kolejne {zadanie}."}, cid)
 
-        player:addOutfit(268, 0)
-        player:addOutfit(269, 0)
+        player:addOutfit(279, 0)
+        player:addOutfit(278, 0)
 
         player:setStorageValue(storageValGood, 1)
       else
@@ -99,9 +91,9 @@ local function creatureSayCallback(cid, type, msg)
       end
     elseif npcHandler.topic[cid] == 11 then
       if player:removeItem(6500, 1000 ) then
-        npcHandler:say({"Niechaj bedzie! Jako mistrz 4konserw, mianuje Cie oficerem pierwszego stopnia.", "Nadany Ci zostaje helmofon, ktory potrafi wykryc neuropejczyka nawet w ciemnosci. Korzystaj z niego madrze."}, cid)
-        player:addOutfitAddon(268, 1)
-        player:addOutfitAddon(269, 1)
+        npcHandler:say({"Gratulacje. Masz ta koske, prosto z izraela, przyda Ci sie do wyjasniania konserwek i lokalnych patriotow w Mirko Town."}, cid)
+        player:addOutfitAddon(278, 1)
+        player:addOutfitAddon(279, 1)
 
         player:setStorageValue(storageValGood, 3)
       else
@@ -109,11 +101,11 @@ local function creatureSayCallback(cid, type, msg)
       end
     elseif npcHandler.topic[cid] == 12 then
       if player:removeItem(6500, 1500 ) then
-        npcHandler:say({"Niechaj bedzie! Jako mistrzu zakonu 4konserw, mianuje Cie starszym oficerem. Jest to najwyzsze odznaczenie.", "Nadane Ci zostaja ta oto wlocznia i tarcza, z wygrawerowanym 3 aksjomatem na wnetrzu. Niech sluzy Ci dobrze."}, cid)
-        player:addOutfitAddon(268, 2)
-        player:addOutfitAddon(269, 2)
+        npcHandler:say({"No sponio, masz tutaj doczepiany kaptur - dzieki temu straznicy nie beda w stanie Cie rozpoznac, gdy bedziesz szczal pod depo.", "Dodatkowo, daje Ci swoja tarcze, podpisana przez mojego idola. Przyda Ci sie gdy wlasciciel bedzie chcial odzyskac naszego squata."}, cid)
+        player:addOutfitAddon(278, 2)
+        player:addOutfitAddon(279, 2)
 
-        if player:addItem(6391, 1, false) then
+        if player:addItem(6433, 1, false) then
           -- ...
           player:setStorageValue(storageValGood, 5)
         else
@@ -130,7 +122,7 @@ local function creatureSayCallback(cid, type, msg)
     npcHandler:say({"To po cholere tu przychodzisz i zawracasz mi glowe?!"}, cid)
     npcHandler.topic[cid] = 0
 	else
-		npcHandler:say("Badz pozdrowiony, jestem mistrzem {Zakonu 4konserw}. Jak moge Ci pomoc?", cid)
+		npcHandler:say("No elo colego, jestem mistrzem {Zakonu Neuropy}. Nie jestes z moderacji, co? Jak moge pomoc?", cid)
 	end
 
 	return true
