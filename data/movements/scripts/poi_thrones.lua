27e199316193852f51a3a6ad441ad657199fa546
Jasiek ~marahin Matusz <me@marahin.pl>
2016-03-11 19:54:04 +0100

    dodane bramy

--- data/movements/scripts/poi_thrones.lua
+++ data/movements/scripts/poi_thrones.lua
@@ -1,4 +1,4 @@
-local function getThronesConquered(player, thrones)
+local function getConqueredThrones(player, thrones)
   local thrones_conquered = 0
 
   for indx, storageKey in ipairs( thrones ) do
@@ -25,15 +25,22 @@ end
 function onStepIn(cid, item, position, fromPosition)
   local player = Player(cid) -- action id used is 37000
   local thrones = { 37001, 37002, 37003, 37004, 37005, 37006, 37007 }
-
-  if player:getStorageValue( item.uid ) >= 1 then
-    -- you already have conquered this throne
-    doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Juz przejales ten tron, pozostalo jeszcze ' .. getThronesLeft(player, thrones) .. '.')
-  else
-    player:setStorageValue(item.uid, 1)
-    -- conquered the throne
-    doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Tron zostal przejety. Pozostalo Ci ' .. getThronesLeft( player, thrones ) .. ' tronow.')
+  if isInArray(thrones, item.uid) -- is it a throne?
+    if player:getStorageValue( item.uid ) >= 1 then
+      -- you already have conquered this throne
+      doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Juz przejales ten tron, pozostalo jeszcze ' .. getThronesLeft(player, thrones) .. '.')
+    else
+      player:setStorageValue(item.uid, 1)
+      -- conquered the throne
+      doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Tron zostal przejety. Pozostalo Ci ' .. getThronesLeft( player, thrones ) .. ' tronow.')
+    end
+  else -- its a gate!
+    if getConqueredThrones(player, thrones) == 7 then
+      -- ok, go on.
+    else
+      doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Masz przejete  ' .. getConqueredThrones(player, thrones) .. ' tronow. Musisz przejac pozostale ' .. getThronesLeft(player, thrones) .. ', aby przejsc dalej.')
+      return false
+    end
   end
-
 	return true
 end
