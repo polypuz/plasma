90dd82301db27972adc6d4db30e072202802f01c
marahin <me@marahin.pl>
2016-03-30 23:34:00 +0200

    wstep do zmian w systemie rajdow

--- src/raids.cpp
+++ src/raids.cpp
@@ -28,6 +28,10 @@
 #include "scheduler.h"
 #include "monster.h"
 
+#include "database.h"
+#include "databasetasks.h"
+
+
 extern Game g_game;
 extern ConfigManager g_config;
 
@@ -49,7 +53,24 @@ Raids::~Raids()
 		delete raid;
 	}
 }
+/* TODO:
+ *
+	/* check if the raid exists in the database */
+/* result != dbResult */
+/* result is the XML result
+ * however dbResult is the result of our query, as the name suggests
+ * Database* db = Database::getInstance();
+ * std::ostringstream query;
+ * query << "SELECT `id` FROM `raids` WHERE `name`=" << db->escapeString( _filename ) << " LIMIT 1";
+ * DBResult_ptr dbResult = db->storeQuery(query.str());
+ * if(!dbResult){
+ * std::cout << "[Error] mirkots_raidsystem: could not receive any results from DB (raid: " <<
+ * _filename.c_str() << ")" << std::endl;
+ * return false;
+ * } */
 
+int64_t lastExecution = dbResult->getNumber<int64_t>("last_exec");
+std::cout << "[Debug] last_execution for raid " << _filename.c_str() << " was (TIMESTAMP) " << lastExecution << std::endl;
 bool Raids::loadFromXml()
 {
 	if (isLoaded()) {
