d2e5f4b6ed1d607acd3b5d3ad53ecd3b610ece49
marahin <me@marahin.pl>
2016-04-02 20:28:02 +0200

    poprawka rozmowy (msgcontains sprawdza pod katem ciagu znakow, a nie slowa jako takiego)

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -15,11 +15,12 @@ local function creatureSayCallback(cid, type, msg)
   if not npcHandler:isFocused(cid) then
     return false
   end
-  if msgcontains(msg, "tytul") or msgcontains(msg, "title") then
-    npcHandler:say({"Tytuly nadawane sa za otrzymanie osiagniecia, ukonczenie wyjatkowo trudnego zadania lub za uczestniczenie w wydarzeniach serwera.", "Jesli chcesz zobaczyc swoje tytuly i zmienic tytul dla swojej postaci, napisz {tytuly}."}, cid)
-  elseif msgcontains(msg, "tytuly") or msgcontains(msg, "titles") then
+
+  if msgcontains(msg, "tytuly") or msgcontains(msg, "titles") then
     print(tostring(getTitles(cid)))
     npcHandler.topic[cid] = 1
+  elseif msgcontains(msg, "tytul") or msgcontains(msg, "title") then
+    npcHandler:say({"Tytuly nadawane sa za otrzymanie osiagniecia, ukonczenie wyjatkowo trudnego zadania lub za uczestniczenie w wydarzeniach serwera.", "Jesli chcesz zobaczyc swoje tytuly i zmienic tytul dla swojej postaci, napisz {tytuly}."}, cid)
   else
     npcHandler:say("Nie rozumiem. Moge wytlumaczyc Ci jak zdobyc {tytul}, albo powiedziec jakie {tytuly} juz zdobyles.", cid)
     npcHandler.topic[cid] = 0
