49d58b2c078bcce09d2d48c5b89a9ddee96d30fe
marahin <me@marahin.pl>
2016-04-02 22:44:41 +0200

    poprawka wypisywania tytulow

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -33,14 +33,11 @@ local function creatureSayCallback(cid, type, msg)
   if msgcontains(msg, "tytuly") or msgcontains(msg, "titles") then
     local titleString = "Aktualnie masz dostepne nastepujace tytuly:"
     local titles = getTitles(cid)
-
     for k, v in ipairs(titles) do
-      titleString = titleString .. " {" .. v.title .. "}"
+      titleString = titleString .. ", [" .. k .. "] {" .. v.title .. "}"
     end
-
     titleString = titleString .. "."
-    npcHandler:say(titleString, cid)
-
+    npcHandler:say({titleString, "Aby zmienic tytul, napisz ktory konkretnie Cie interesuje (np. {1})."}, cid)
     npcHandler.topic[cid] = 1
   elseif msgcontains(msg, "tytul") or msgcontains(msg, "title") then
     npcHandler:say({"Tytuly nadawane sa za otrzymanie osiagniecia, ukonczenie wyjatkowo trudnego zadania lub za uczestniczenie w wydarzeniach serwera.", "Jesli chcesz zobaczyc swoje tytuly i zmienic tytul dla swojej postaci, napisz {tytuly}."}, cid)
