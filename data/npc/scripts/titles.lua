59a01c7da079b3c85528a6d697d7a81e113138eb
marahin <me@marahin.pl>
2016-04-02 20:18:23 +0200

    testujemy, czy działa

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -1,23 +1,18 @@
---
--- Created by IntelliJ IDEA.
--- User: marahin
--- Date: 02.04.16
--- Time: 18:50
--- To change this template use File | Settings | File Templates.
---
-
 local keywordHandler = KeywordHandler:new()
 local npcHandler = NpcHandler:new(keywordHandler)
 NpcSystem.parseParameters(npcHandler)
 
-function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
-function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
-function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
-function onThink() npcHandler:onThink() end
-
+function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
+function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
+function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
+function onThink()				npcHandler:onThink()					end
 
 local function creatureSayCallback(cid, type, msg)
-  npcHandler:say("penis", cid)
-
+  if not npcHandler:isFocused(cid) then
+    return false
+  end
+  npcHandler:say("test", cid)
   return true
 end
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
