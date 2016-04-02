d1e6fc6c574fac4f0776953b66af4761653367ca
marahin <me@marahin.pl>
2016-04-02 20:25:08 +0200

    debugujemy czym jest results

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -7,8 +7,8 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
-local function getTitles()
-  return true
+local function getTitles(cid)
+  return db.storeQuery("SELECT `title_id` FROM `player_titles` WHERE `account_id`=" .. Player(cid):getAccountId())
 end
 
 local function creatureSayCallback(cid, type, msg)
@@ -18,7 +18,7 @@ local function creatureSayCallback(cid, type, msg)
   if msgcontains(msg, "tytul") or msgcontains(msg, "title") then
     npcHandler:say({"Tytuly nadawane sa za otrzymanie osiagniecia, ukonczenie wyjatkowo trudnego zadania lub za uczestniczenie w wydarzeniach serwera.", "Jesli chcesz zobaczyc swoje tytuly i zmienic tytul dla swojej postaci, napisz {tytuly}."}, cid)
   elseif msgcontains(msg, "tytuly") or msgcontains(msg, "titles") then
-    getTitles()
+    print(tostring(getTitles(cid)))
     npcHandler.topic[cid] = 1
   else
     npcHandler:say("Nie rozumiem. Moge wytlumaczyc Ci jak zdobyc {tytul}, albo powiedziec jakie {tytuly} juz zdobyles.", cid)
