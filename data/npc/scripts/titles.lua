69548939320bf64cd0ea7a78ec677f4e0a8e0529
marahin <me@marahin.pl>
2016-04-02 20:22:18 +0200

    szkielet rozmowy

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -7,11 +7,24 @@ function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
 function onThink()				npcHandler:onThink()					end
 
+local function getTitles()
+  return true
+end
+
 local function creatureSayCallback(cid, type, msg)
   if not npcHandler:isFocused(cid) then
     return false
   end
-  npcHandler:say("test", cid)
+  if msgcontains(msg, "tytul") or msgcontains(msg, "title") then
+    npcHandler:say({"Tytuly nadawane sa za otrzymanie osiagniecia, ukonczenie wyjatkowo trudnego zadania lub za uczestniczenie w wydarzeniach serwera.", "Jesli chcesz zobaczyc swoje tytuly i zmienic tytul dla swojej postaci, napisz {tytuly}."}, cid)
+  elseif msgcontains(msg, "tytuly") or msgcontains(msg, "titles") then
+    getTitles()
+    npcHandler.topic[cid] = 1
+  else
+    npcHandler:say("Nie rozumiem. Moge wytlumaczyc Ci jak zdobyc {tytul}, albo powiedziec jakie {tytuly} juz zdobyles.", cid)
+    npcHandler.topic[cid] = 0
+  end
+
   return true
 end
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
