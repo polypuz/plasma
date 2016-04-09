9ead4793f4bb801986ee939428c74bf26cc445c9
marahin <me@marahin.pl>
2016-04-09 22:46:18 +0200

    poprawienie brakujacego segmentu parameters

--- data/npc/scripts/titles.lua
+++ data/npc/scripts/titles.lua
@@ -5,7 +5,7 @@ NpcSystem.parseParameters(npcHandler)
 function onCreatureAppear(cid)			npcHandler:onCreatureAppear(cid)			end
 function onCreatureDisappear(cid)		npcHandler:onCreatureDisappear(cid)			end
 function onCreatureSay(cid, type, msg)		npcHandler:onCreatureSay(cid, type, msg)		end
-function onThink()				npcHandler:onThink() end
+function onThink()				npcHandler:onThink()					end
 
 --[[
 --getTitles()
@@ -65,5 +65,6 @@ local function creatureSayCallback(cid, type, msg)
 
   return true
 end
+
 npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
 npcHandler:addModule(FocusModule:new())
