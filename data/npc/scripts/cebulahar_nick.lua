2561fc0aa152037cc6da4b70360e97f0816e5947
marahin <me@marahin.pl>
2016-04-10 17:32:06 +0200

    troche erpega u Straznika Nicka

--- data/npc/scripts/cebulahar_nick.lua
+++ data/npc/scripts/cebulahar_nick.lua
@@ -59,13 +59,14 @@ local function creatureSayCallback(cid, type, msg)
     end
   elseif npcHandler.topic[cid] == 2 then
     if msgcontains(msg, "tak") or msgcontains(msg, "yes") then
-      npcHandler:say({"Tak myslalem. Wspominal mi, glupek jeden...", "Dobra, przelaz. Tylko nikomu ani slowa, bo mnie wyleja, a niedawno zniesiono ustawe 500+, nie bedzie mial kto mlodych utrzymac. Jak bedziesz chcial wrocic do {miasta}, to powiedz po prostu {powrot}."}, cid)
-      doTeleportThing(cid, district)
+      npcHandler:say({"Tak myslalem. Wspominal mi, glupek jeden...", "Dobra, przelaz. Tylko nikomu ani slowa, bo mnie wyleja, a niedawno zniesiono ustawe 500+, nie bedzie mial kto mlodych utrzymac. Jak bedziesz chcial wrocic do {miasta}, to powiedz po prostu {powrot}. *wyjmuje klucze do bramy*"}, cid)
+      addEvent(doTeleportThing(cid, district), 3000)
     else
       npcHandler:say("Niewazne. Spadaj, jestem na warcie.", cid)
     end
   elseif msgcontains(msg, "powrot") or msgcontains(msg, "miasta") or msgcontains(msg, "miasto") then
-    doTeleportThing(cid, city)
+    npcHandler:say("W porzadku, przelaz, tylko szybko... i nikomu nic nie mow. *wyjmuje klucze do bramy*")
+    addEvent(doTeleportThing(cid, city), 3000)
   else
     npcHandler:say("Nie wiem o co chodzi, obywatelu. Prosze odejsc.", cid)
   end
