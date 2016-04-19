743dbb5a4d8fb24d92178f7ecabaceabeb79ed5a
marahin <me@marahin.pl>
2016-04-19 20:42:39 +0200

    (proba naprawienia) Mag Natt poprawki

--- data/npc/scripts/natt.lua
+++ data/npc/scripts/natt.lua
@@ -6,6 +6,15 @@
 -- To change this template use File | Settings | File Templates.
 --
 
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid) npcHandler:onCreatureAppear(cid) end
+function onCreatureDisappear(cid) npcHandler:onCreatureDisappear(cid) end
+function onCreatureSay(cid, type, msg) npcHandler:onCreatureSay(cid, type, msg) end
+function onThink() npcHandler:onThink() end
+
 
 local function greetCallback(cid)
   -- npcHandler:setMessage(MESSAGE_GREET, msg, cid)
